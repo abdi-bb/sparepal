68be43b6d416dd8fee85b73cb9ea012b93aa2055
Abdi Berhe <abdiberhe@gmail.com>
2024-11-29 15:19:50 +0300

    Override SocialLoginSerializer
    Override SocialLoginSerializer to fix compatiblity issue between
    django_allath>0.61.1 and dj-rest-auth. Make dj-rest-auth to use
    get_scope_from_request inplace of get_scope.

--- sparepal/users/api/serializers.py
+++ sparepal/users/api/serializers.py
@@ -1,8 +1,20 @@
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.registration.serializers import SocialLoginSerializer
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext as _
+from django.db import IntegrityError
+from django.http import HttpResponseBadRequest
+from django.utils.translation import gettext_lazy as _
+from requests.exceptions import HTTPError
 from rest_framework import serializers
+
+try:
+    from allauth.account import app_settings as allauth_account_settings
+    from allauth.socialaccount.helpers import complete_social_login
+except ImportError as err:
+    ALLAUTH_MISSING_ERROR = "allauth needs to be added to INSTALLED_APPS."
+    raise ImportError(ALLAUTH_MISSING_ERROR) from err
 
 from sparepal.users.models import Profile
 
@@ -157,3 +169,134 @@ class CustomUserLoginSerializer(serializers.Serializer):
         validated_data["user"] = user
 
         return validated_data
+
+
+# Fix compatiblity issue between django_allath>0.61.1 and dj-rest-auth
+# Make dj-rest-auth to use get_scope_from_request inplace of get_scope
+
+
+# ruff: noqa: C901, PLR0912, PLR0915
+class CustomSocialLoginSerializer(SocialLoginSerializer):
+    def validate(self, attrs):
+        view = self.context.get("view")
+        request = self._get_request()
+
+        if not view:
+            raise serializers.ValidationError(
+                _("View is not defined, pass it as a context variable"),
+            )
+
+        adapter_class = getattr(view, "adapter_class", None)
+        if not adapter_class:
+            raise serializers.ValidationError(_("Define adapter_class in view"))
+
+        adapter = adapter_class(request)
+        app = adapter.get_provider().app
+
+        # More info on code vs access_token
+        # http://stackoverflow.com/questions/8666316/facebook-oauth-2-0-code-and-token
+
+        access_token = attrs.get("access_token")
+        code = attrs.get("code")
+        # Case 1: We received the access_token
+        if access_token:
+            tokens_to_parse = {"access_token": access_token}
+            token = access_token
+            # For sign in with apple
+            id_token = attrs.get("id_token")
+            if id_token:
+                tokens_to_parse["id_token"] = id_token
+
+        # Case 2: We received the authorization code
+        elif code:
+            self.set_callback_url(view=view, adapter_class=adapter_class)
+            self.client_class = getattr(view, "client_class", None)
+
+            if not self.client_class:
+                raise serializers.ValidationError(
+                    _("Define client_class in view"),
+                )
+
+            provider = adapter.get_provider()
+            scope = provider.get_scope_from_request(request)
+            client = self.client_class(
+                request,
+                app.client_id,
+                app.secret,
+                adapter.access_token_method,
+                adapter.access_token_url,
+                self.callback_url,
+                scope,
+                headers=adapter.headers,
+                basic_auth=adapter.basic_auth,
+            )
+            try:
+                token = client.get_access_token(code)
+            except OAuth2Error as ex:
+                raise serializers.ValidationError(
+                    _("Failed to exchange code for access token"),
+                ) from ex
+            access_token = token["access_token"]
+            tokens_to_parse = {"access_token": access_token}
+
+            # If available we add additional data to the dictionary
+            for key in ["refresh_token", "id_token", adapter.expires_in_key]:
+                if key in token:
+                    tokens_to_parse[key] = token[key]
+        else:
+            raise serializers.ValidationError(
+                _("Incorrect input. access_token or code is required."),
+            )
+
+        social_token = adapter.parse_token(tokens_to_parse)
+        social_token.app = app
+
+        try:
+            if adapter.provider_id == "google" and not code:
+                login = self.get_social_login(
+                    adapter,
+                    app,
+                    social_token,
+                    response={"id_token": id_token},
+                )
+            else:
+                login = self.get_social_login(adapter, app, social_token, token)
+            ret = complete_social_login(request, login)
+        except HTTPError as err:
+            incorrect_value_err = "An error occurred: Incorrect value"
+            raise serializers.ValidationError(_(incorrect_value_err)) from err
+
+        if isinstance(ret, HttpResponseBadRequest):
+            raise serializers.ValidationError(ret.content)
+
+        if not login.is_existing:
+            # We have an account already signed up in a different flow
+            # with the same email address: raise an exception.
+            # This needs to be handled in the frontend. We can not just
+            # link up the accounts due to security constraints
+            if allauth_account_settings.UNIQUE_EMAIL:
+                # Do we have an account already with this email address?
+                account_exists = (
+                    get_user_model()
+                    .objects.filter(
+                        email=login.user.email,
+                    )
+                    .exists()
+                )
+                if account_exists:
+                    raise serializers.ValidationError(
+                        _("User is already registered with this e-mail address."),
+                    )
+
+            login.lookup()
+            try:
+                login.save(request, connect=True)
+            except IntegrityError as ex:
+                raise serializers.ValidationError(
+                    _("User is already registered with this e-mail address."),
+                ) from ex
+            self.post_signup(login, attrs)
+
+        attrs["user"] = login.account.user
+
+        return attrs
