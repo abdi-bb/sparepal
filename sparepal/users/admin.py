7a19fb24b99c8bec23d5d8e49b45a827bde79b44
Abdi Berhe <abdiberhe@gmail.com>
2024-11-29 14:45:22 +0300

    Enable secure admin login
    Enable secure admin login, by forcing it to go thrugh django-allauth.

--- sparepal/users/admin.py
+++ sparepal/users/admin.py
@@ -1,3 +1,5 @@
+from allauth.account.decorators import secure_admin_login
+from django.conf import settings
 from django.contrib import admin
 
 # Register your models here.
@@ -6,6 +8,12 @@ from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 from .models import Profile
+
+if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
+    # Force the `admin` sign in process to go through the `django-allauth` workflow:
+    # https://docs.allauth.org/en/latest/common/admin.html#admin
+    admin.autodiscover()
+    admin.site.login = secure_admin_login(admin.site.login)  # type: ignore[method-assign]
 
 
 @admin.register(CustomUser)
